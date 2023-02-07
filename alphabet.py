3b13378d626273d04190050c62e717063d0d8136
Quentin <damevinq@gmail.com>
2023-02-07 15:41:22 +0100

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,6 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-for i in range(1):
-    print(alphabet)
-    print()
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u","v", "w", "x", "y", "z"]
 
+for i in alphabet:
+    print(i, end="")
+    
+print()
